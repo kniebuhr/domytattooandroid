96e7beeae7851cdf5b47894c5cdc09925984da59
kniebuhr <gk.niebuhr@gmail.com>
2017-11-11 13:41:47 -0200

    ssd ccaralho

---
+++ app/src/main/java/ink/domytattoo/SearchFragment.kt
@@ -0,0 +1,34 @@
+package ink.domytattoo
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BlankFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        var rootView = inflater?.inflate(R.layout.fragment_search, container, false)
+        return rootView
+    }
+
+
+//    companion object {
+//
+//        private val ARG_PARAM1 = "param1"
+//        private val ARG_PARAM2 = "param2"
+//
+//        // TODO: Rename and change types and number of parameters
+//        fun newInstance(param1: String, param2: String): BlankFragment {
+//            val fragment = BlankFragment()
+//            val args = Bundle()
+//            args.putString(ARG_PARAM1, param1)
+//            args.putString(ARG_PARAM2, param2)
+//            fragment.arguments = args
+//            return fragment
+//        }
+//    }
+}
